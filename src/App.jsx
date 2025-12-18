e92e2b19bde2c523b80b04dadf8bfb05c109cef0
nikita thanki <thankinikita32@gmail.com>
2025-12-18 19:05:34 +0530

    project

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { useState } from 'react'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './Pages/Login';
-import Register from './Pages/Register ';
+import Register from './Pages/Register';
 
 function App() {
   const router = createBrowserRouter([
-    {
-      path: "/login",
-      element: <Login />
-    },
-     {
-      path: "/register",
-      element: <Register />
-    }
-  ]);
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+]);
+
 
   return <RouterProvider router={router} />;
 }
