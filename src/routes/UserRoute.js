550d0adf417960a1e859c4897634aa538f61e5c9
nikita thanki <thankinikita32@gmail.com>
2025-12-18 09:50:52 +0530

    project setup

--- src/routes/UserRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/UserController");
-
-router.get("/list", userController.getList);
-
-module.exports = router;
