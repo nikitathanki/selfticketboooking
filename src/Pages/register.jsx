9762cfed849c2d3d81555b7b9285c7bf7e44fd72
nikita thanki <thankinikita32@gmail.com>
2025-12-18 11:46:35 +0530

    project setup

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,6 +1,15 @@
+import { Phone } from "lucide-react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 const Register = () => {
+    const {formData, setformData}= useState({
+        name:"",
+        email:"",
+        Phone:"",
+        password:""
+    });
+const [erros, setErrors]= useState({});
 const navigate = useNavigate()
 
   return (
@@ -33,6 +42,7 @@ const navigate = useNavigate()
                         <div className="relative mt-1">
                             <input
                                 type="text"
+                                value={formData.name}
                                 placeholder="John Doe"
                                 className="w-full pl-4 pr-4 py-3 bg-gray-100 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-orange-400"
                             />
