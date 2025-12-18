e01e4f29b5f04853e8759a7172dfd2f2c282c5a1
nikita thanki <thankinikita32@gmail.com>
2025-12-18 10:20:01 +0530

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
+import Login from '/Pages/Login.jsx';
+import Register from '/Pages/Register.jsx';
 
 function App() {
   const router = createBrowserRouter([
